4ba4e8434dbe18f7e9284b3cd07fe7d2af6f0687
Munna4 <73132674+freelancermunna4@users.noreply.github.com>
2022-12-07 00:32:11 +0600

    Update aprove-product.php

--- aprove-product.php
+++ aprove-product.php
@@ -67,7 +67,7 @@
                                       <?php  }?>
 
                                 </td>
-                            </tr>
+                            </tr> 
 
                             <?php }?>
                         </tbody>
