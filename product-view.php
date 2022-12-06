d3f566d0ddc3c81348ffb07d204fea255e9e1134
Munna4 <73132674+freelancermunna4@users.noreply.github.com>
2022-12-06 13:26:14 +0600

    Update product-view.php

--- product-view.php
+++ product-view.php
@@ -27,7 +27,7 @@
                             d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
                     </svg>
                 </div> 
-                <div class="notify_text">
+                <div class="notify_text"> 
                     <small><?php
                $d=_getData($db,"SELECT * FROM `notice` WHERE name='web'");
                echo $d['notice'];
